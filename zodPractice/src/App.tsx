df5a2cf1c1ff295484142dca357ea73fe190ddc9
Ričardas Staniulis <r.staniulis@ba.lt>
2025-06-23 10:44:25 +0300

    add onChange

--- zodPractice/src/App.tsx
+++ zodPractice/src/App.tsx
@@ -1,21 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleOnChangeEvent = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    state: React.Dispatch<React.SetStateAction<string>>
+  ) => {
+    state(e.target.value);
+  };
+
+  console.log(email);
+
   return (
     <>
       <form className="flex flex-col justify-center items-center">
         <input
+          value={email}
+          onChange={(e) => handleOnChangeEvent(e, setEmail)}
           type="email"
           placeholder="Email"
           className="border-black border-2 m-2 p-2"
         />
         <input
           type="password"
+          value={password}
+          onChange={(e) => handleOnChangeEvent(e, setPassword)}
           placeholder="Password"
           className="border-black border-2 m-2 p-2"
         />
         <input
           type="password"
+          value={confirmPassword}
+          onChange={(e) => handleOnChangeEvent(e, setConfirmPassword)}
           placeholder="Confirm Password"
           className="border-black border-2 m-2 p-2"
         />
